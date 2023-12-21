b6a5776e71116cc7240504befa19df13a8f5d8d0
Ksew <vlassovaksenia@gmail.com>
2023-12-21 20:52:24 +0100

    add generating report

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,8 @@ module.exports = {
   "reporters": [
       "default",
       ["jest-html-reporters", {
-        "publicPath": "./html-report",
-        "filename": "report.html",
-        "expand": true
+        outputPath: 'test-results/e2e_tests/report.html',
+        includeFailureMsg: true
       }]
     ]
 };
