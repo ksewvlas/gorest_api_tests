90d6ad2610fe3ccd11370f42a308841fed121edd
Ksew <vlassovaksenia@gmail.com>
2023-12-21 19:33:45 +0100

    Add e2e tests for user CRUD operations

---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+const getApiConfig = () => ({
+  baseUrl: "https://gorest.co.in/public/v2",
+  validToken:
+    "988e9eebccdded5318cf006cf32ea8f00b46d6ce3e83769d88f40a47ebc19f4c",
+  invalidToken: "invalidToken",
+});
+
+module.exports = {
+  getApiConfig,
+};
