41da1414f6dc0906183363de6e32a232fae5acca
Ksew <vlassovaksenia@gmail.com>
2023-12-22 00:13:35 +0100

    add generating report

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/?(*.)+(spec|test).js"],
-  setupFiles: ["./jest.setup.js"],
-  reporters: [
-    "default",
-    [
-      "jest-html-reporters",
-      {
-        publicPath: "./test_results/jest-html-reporters-attach",
-        filename: "report.html",
-        includeFailureMsg: true,
-      },
-    ],
-  ],
+  setupFiles: ["./jest.setup.js"]
+  // reporters: [
+  //   "default",
+  //   [
+  //     "jest-html-reporters",
+  //     {
+  //       publicPath: "./test_results/jest-html-reporters-attach",
+  //       filename: "report.html",
+  //       includeFailureMsg: true,
+  //     },
+  //   ],
+  // ],
 };
