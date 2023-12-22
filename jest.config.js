726a891b9179afebe312490df0aaf508b60e7edd
ksewvlas <43511300+ksewvlas@users.noreply.github.com>
2023-12-22 01:00:10 +0000

    Deploying to gh-pages from  @ a50246b5039ff55856274d7a0cfa79dda0d51d1b 🚀

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/?(*.)+(spec|test).js"],
-  setupFiles: ["./jest.setup.js"],
-  reporters: ["default", "jest-html-reporters"],
-  collectCoverage: true,
-  coverageReporters: ["html"],
-};
