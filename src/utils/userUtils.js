5f85d488a03effcaaa3257389dcfe79c3992a0f7
Xeniya Vlassova <43511300+ksewvlas@users.noreply.github.com>
2023-12-22 11:17:04 +0100

    fix CI configuration (#3)
    * add configuration for ci cd
    
    * fix error in ci configuration
    
    * fix error in ci configuration
    
    * add linters and formatters for ci configuration
    
    * fix remove comments
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * add generating report
    
    * fix ci configuration

--- src/utils/userUtils.js
+++ src/utils/userUtils.js
@@ -22,8 +22,37 @@ async function cleanupUser(userId) {
   await deleteUser(userId);
 }
 
+const wait = async (milliseconds) =>
+  // eslint-disable-next-line no-undef
+  new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+const waitForSuccessfulResponse = async (
+  requestFunction,
+  maxAttempts = 5,
+  waitTime = 1000,
+) => {
+  let attempt = 0;
+  while (attempt < maxAttempts) {
+    try {
+      const response = await requestFunction();
+      if (response.status === 200) {
+        return response.body;
+      }
+    } catch (error) {
+      /* empty */
+    }
+    await wait(waitTime);
+    attempt++;
+  }
+  throw new Error(
+    `Maximum attempts (${maxAttempts}) reached without a successful response.`,
+  );
+};
+
 module.exports = {
   cleanupUser,
   generateRandomUserData,
   generateInvalidUserDataWithoutRequiredField,
+  waitForSuccessfulResponse,
+  wait,
 };
